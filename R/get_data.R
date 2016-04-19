80f3a35204b30d99f85b40b9cc2a562fae2a8889
Patrick Hausmann <patrick.hausmann@covimo.de>
2016-04-19 17:19:03 +0200

    using '::'

--- R/get_data.R
+++ R/get_data.R
@@ -4,9 +4,7 @@
 
 get_data <- function(url) {
   # url <- "http://referendum2016.comune.palermo.it/AFFLSEZ_1_82053_R1.xml"
-  x <- read_xml(url) %>%
-    xml_nodes('SV') %>%
-    html_attrs()
+  x <- xml2::read_xml(url) %>% rvest::xml_nodes('SV') %>% rvest::html_attrs()
 
   x <- data.frame(do.call("rbind", x), stringsAsFactors = FALSE)
   x$TOTVOT <- as.numeric(x$TOTVOT)

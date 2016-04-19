4981bd3df9dee44beb4695e668ac57782f6adf00
Patrick Hausmann <patrick.hausmann@covimo.de>
2016-04-19 16:18:50 +0200

    back to import

--- R/refpal-package.R
+++ R/refpal-package.R
@@ -5,7 +5,7 @@
 #' @name refpal-package
 #' @docType package
 #' @author Patrick Hausmann \email{patrick.hausmann@@covimo.de}
-#' @importFrom xml2 read_xml
+#' @import xml2
 #' @import rvest
 #' @import magrittr
 NULL
